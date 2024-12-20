ea6c85de13eedbcb8f1744c507d15ecbab448b9d
Samadessouky <128940285+Samadessouky@users.noreply.github.com>
2024-12-20 02:37:29 +0200

    Update tests.py

--- events/tests.py
+++ events/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 from .models import EventCategory
 
 class EventCategoryTests(TestCase):

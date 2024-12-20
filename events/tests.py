1b1fee9ed681ed0db9ab2c7f2553f835215e7112
Samadessouky <128940285+Samadessouky@users.noreply.github.com>
2024-12-20 02:24:30 +0200

    Update tests.py

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
 
-# Create your tests here.
+
+from django.test import TestCase
+from django.urls import reverse
+from .models import EventCategory
+
+class EventCategoryTests(TestCase):
+
+    def test_event_category_creation(self):
+        category = EventCategory.objects.create(name="Test Category", code="TC01", status="active")
+        self.assertEqual(category.name, "Test Category")
+        self.assertEqual(category.code, "TC01")
+
+    def test_event_category_list_view(self):
+        response = self.client.get(reverse('admin_events:event-category-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Category")
+
